76cf92cea2df8e4c997e9dd54fb7615875295a7b
tejaprf <bollatejaprf@gmail.com>
2024-07-22 22:16:50 +0530

    Update mongoConnect.js

--- backend/db/mongoConnect.js
+++ backend/db/mongoConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const mongoCon=async () =>{
     try {
-        await mongoose.connect(process.env.mongoUrl);
+        await mongoose.connect(process.env.mongoUrlProd);
         console.log("Connected to mongoDB",process.env.mongoUrlProd);
     } catch (error) {
         console.log("Connection to DB failed",error.message);
